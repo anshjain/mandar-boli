c02acc335bb2d23ee8d741c5b96b96a91f4e4d83
Priya Jain <2442016priya@gmail.com>
2018-06-25 00:28:54 +0530

    Update logic to search based on temple

--- mandir/forms.py
+++ mandir/forms.py
@@ -3,11 +3,16 @@ from __future__ import unicode_literals
 
 from django import forms
 
-from mandir.models import BoliChoice
+from mandir.models import BoliChoice, Mandir
 
 
 class SearchForm(forms.Form):
     phone_number = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Phone Number', 'autocomplete': 'off'}))
+    mandir = forms.ModelChoiceField(widget=forms.Select(
+        attrs={
+            'class': 'w3-input w3-border',
+            'style': 'width: 20%; float: right; margin: 3px; height: 38px;'
+        }), queryset=Mandir.objects.all(), required=True)
 
 
 class EntryForm(forms.Form):
