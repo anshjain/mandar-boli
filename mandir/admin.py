a15fe28c2be4aa05cd29df1e0ef4ae6e12ad20c8
Priya Jain <2442016priya@gmail.com>
2018-06-28 10:55:18 +0530

    update admin and staff view

--- mandir/admin.py
+++ mandir/admin.py
@@ -2,11 +2,26 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
 from mandir.models import Mandir, Record, MandirImage, BoliChoice
 
 
 class MandirAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'contract_number', 'email')
+    readonly_fields = ('user',)
+
+    def get_queryset(self, request):
+        """Limit records to those that belong to the user temple."""
+
+        qs = super(MandirAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            # It is mine, all mine. Just return everything.
+            return qs
+        # Now we just add an extra filter on the queryset and
+        # we're done. Assumption: Page.owner is a foreignkey
+        # to a User.
+        return qs.filter(user=request.user)
 
 
 class RecordAdmin(admin.ModelAdmin):
@@ -29,6 +44,18 @@ class RecordAdmin(admin.ModelAdmin):
     #Filtering on side - for some reason, this works
     list_filter = ['title', 'paid', 'account__phone_number']
 
+    def get_queryset(self, request):
+        """Limit records to those that belong to the user temple."""
+
+        qs = super(RecordAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            # It is mine, all mine. Just return everything.
+            return qs
+        # Now we just add an extra filter on the queryset and
+        # we're done. Assumption: Page.owner is a foreignkey
+        # to a User.
+        return qs.filter(mandir=request.user.mandir)
+
 
 class MandirImageAdmin(admin.ModelAdmin):
     list_display = ('get_mandir_name',)
@@ -36,6 +63,18 @@ class MandirImageAdmin(admin.ModelAdmin):
     def get_mandir_name(self, obj):
         return obj.mandir.name
     get_mandir_name.short_description = 'Mandir Name'
+
+    def get_queryset(self, request):
+        """Limit records to those that belong to the user temple."""
+
+        qs = super(MandirImageAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            # It is mine, all mine. Just return everything.
+            return qs
+        # Now we just add an extra filter on the queryset and
+        # we're done. Assumption: Page.owner is a foreignkey
+        # to a User.
+        return qs.filter(mandir=request.user.mandir)
 
 
 class BoliChoiceAdmin(admin.ModelAdmin):
