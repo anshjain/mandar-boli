82f1f6034d1b68596ff79b64b9c49474d9df0aac
Priya Jain <2442016priya@gmail.com>
2018-07-01 00:57:01 +0530

    Add about us and contact us static pages

--- Boli/urls.py
+++ Boli/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url, static
 
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
 
 
 from mandir.views import RecordListView, EntryCreateView, ajax_single_account
@@ -30,6 +31,8 @@ urlpatterns = [
     url(r'^search/$', RecordListView.as_view(), name='record-list'),
     url(r'^add/$', EntryCreateView.as_view(), name='add-record'),
     url(r'^get/description/$', ajax_single_account, name='des-search'),
+    url(r'^about-us/$', TemplateView.as_view(template_name="about_us.html"), name='about'),
+    url(r'^contact-us/$', TemplateView.as_view(template_name="contact.html"), name='contact-us'),
 ] + static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
               static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
