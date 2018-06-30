82446f9a0ecfb874c783142de852852c795b5ba2
Priya Jain <2442016priya@gmail.com>
2018-06-30 13:02:01 +0530

    update logic for error logging

--- mandir/forms.py
+++ mandir/forms.py
@@ -15,10 +15,17 @@ class EntryForm(forms.Form):
                                                               'style': 'height: 40px;'}),
                                    queryset=BoliChoice.objects.all(),
                                    required=True)
-    phone_number = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Phone Number',
-                                                                 'autocomplete': 'off', 'class': 'w3-input w3-border'}))
+    phone_number = forms.RegexField(regex=r'^\?1?\d{9,15}$', max_length=10,
+                                    widget=forms.TextInput(
+                                        attrs={'error_message': ("Phone number must be entered in the format: "
+                                                                 "'9999999999'.Up to 15 digits allowed."),
+                                               'placeholder': 'Phone Number',
+                                               'autocomplete': 'off', 'class': 'w3-input w3-border'}
+                                    )
+    )
+
     description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'description', 'autocomplete': 'off',
-                                                               'class': 'w3-input w3-border', 'rows': '3'}))
-    amount = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'amount', 'autocomplete': 'off',
-                                                           'class': 'w3-input w3-border',
-                                                           'style': 'margin: 18px 6px 0px 6px'}))
+                                                               'class': 'w3-input w3-border', 'rows': '4'}))
+    amount = forms.CharField(widget=forms.NumberInput(attrs={'placeholder': 'amount', 'autocomplete': 'off',
+                                                             'class': 'w3-input w3-border',
+                                                             'style': 'margin: 18px 6px 0px 6px'}))
