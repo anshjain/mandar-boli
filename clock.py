5d6291cbeb625362dabd4094a605075f409f667c
Priya Jain <2442016priya@gmail.com>
2018-10-20 15:50:37 +0530

    Update print status

--- clock.py
+++ clock.py
@@ -8,13 +8,13 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day='20', hour=10, minute=25)
 def record_csv_job():
-    print "Start con"
+    print ("Start con")
     subprocess.call('python ./manage.py recordscsv', shell=True, close_fds=True)
 
 
 # @sched.scheduled_job('cron', day='9', hour=13, minute=30)
 # def send_reminder_job():
-#     print "Start send message con!"
+#     print ("Start send message con!")
 #     subprocess.call('python ./manage.py sendmessage', shell=True, close_fds=True)
 
 
