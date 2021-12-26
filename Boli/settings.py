a2d111d11d0b1b01bf1523c0225dd5e29e7c161f
Shreeyansh Jain <jain.scs@gmail.com>
2021-12-26 17:09:15 +0530

    Update settings.py

--- Boli/settings.py
+++ Boli/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's-cl_(s$*lcg7$8sv$ivfg1$%mcnbexouu+gqt15wt-bj(v_)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 SEND_SMS = True
 
 ALLOWED_HOSTS = ["punyaudayfund.herokuapp.com", '127.0.0.1', 'www.susjainmandir.com', 'susjainmandir.com']
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'Boli.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
